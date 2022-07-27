8046928a2a56e55f53c059658ced47db5042cf3e
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-27 23:02:23 +0300

    states the component and lins wit api

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
 import './App.css';
 import { Component } from 'react';
+import Home from '../src/components/Home';
+import { Router, Route } from "react-router-dom"
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       items: [],
       isLoaded: false,
-  }
+    }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=f')
       .then(res => res.json())
       .then(json => {
         this.setState({
-          isLoaded:true,
-          items:json.Meals
+          isLoaded: true,
+          items: json.meals
         })
       });
   }
 
-render(){
-  let { isLoaded, items } = this.state;
-  if (!isLoaded){
-    return(
+  render() {
+    let { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return (
+        <div>
+          loading..
+        </div>)
+    } else {
+      return (
+        <div className="App">
+          <center>
+            <ul>
+              {items.map(item => (
+                <li key={item.idMeal}>
+                  Meal: {item.strMeal} Category: {item.strCategory}
+                  Image :<img src={item.strMealThumb} style={{ height: 200 + `px`, width: 200 + `px` }}></img>
+                  Recipe :{item.strInstructions}
+
+                </li>
+              ))};
+            </ul>
+          </center>
+        </div>
+      );
+    }
+return(
+  <Router>
     <div>
-      loading..
-    </div>)
-  }else{
-    return (
-      <div className="App">
-        <ul>
-          { items.map(item => (
-            <li key={item.idMeal}>
-            Meal: {item.strMeal} Category: {item.strCategory}
-            </li>
-          )) };
-        </ul>
-      </div>
-    );
+    <Home/>
+    </div>
+  </Router>
+)
   }
-  
-}
-}
+};
 
 export default App;

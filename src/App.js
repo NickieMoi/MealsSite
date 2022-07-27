77c6504300fc8c837f3e959720f620bc578e956b
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-27 20:37:40 +0300

    modifies the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+      isLoaded: false,
+  }
+  }
+
+  componentDidMount(){
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=f')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded:true,
+          items:json.Meals
+        })
+      });
+  }
+
+render(){
+  let { isLoaded, items } = this.state;
+  if (!isLoaded){
+    return(
+    <div>
+      loading..
+    </div>)
+  }else{
+    return (
+      <div className="App">
+        <ul>
+          { items.map(item => (
+            <li key={item.idMeal}>
+            Meal: {item.strMeal} Category: {item.strCategory}
+            </li>
+          )) };
+        </ul>
+      </div>
+    );
+  }
+  
+}
 }
 
 export default App;

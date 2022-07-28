5427c58925537a7856e1fd1871abc2e9d303e2cd
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-28 15:46:32 +0300

    adds the home file component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 
 
-function Home(){
-return (
-    <div>
-        <h2>MyMealSite
-        </h2>
+class Home extends Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          items: this.props.items,
+          value: [],
+        }
 
-    </div>
-)
+        this.handleChange =this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
 
+      handleChange(event){
+        this.setState({value:event.target.idMeal});
+      }
+
+      handleSubmit(event){
+        console.log(this.state.value)
+        event.preventDefault();
+      }
+
+render(){
+    let items=this.props.items
+
+
+    return (
+        <React.Fragment>
+        <div>
+          <div className="App">
+              <div id='header'>
+                My<span>Meal</span>pal
+              </div>
+              <div id='head'>
+                <ul>
+                  {items.map(item => (
+                    <li key={item.idMeal}>
+                      <li>Meal: <b>{item.strMeal}</b> </li>
+                      <li>Category: <i>{item.strCategory}</i></li>
+                      <li><img src={item.strMealThumb} style={{ height: 100 + `%`, width: 100 + `%` }}></img></li>
+                      <li id='last'><b>Recipe</b> :<p>{item.strInstructions}</p></li>
+                      <form onSubmit={this.handleSubmit}>
+                      <li className='list'><input class="form-control" type="text" placeholder="Add review here" value={this.state.value} onChange={this.handleChange}></input><button type='submit' className='btn btn-lg btn-success'>Submit</button></li></form>
+                      <li><button type='button' className='btn btn-md btn-outline-success order'>Order</button></li>
+                      <li id='buttons'><img src="https://img.icons8.com/emoji/38/000000/thumbs-up.png"/><img src="https://img.icons8.com/emoji/38/000000/thumbs-down-emoji.png"/></li>
+    
+                    </li>
+                  ))};
+                </ul>
+            </div>
+            </div>
+        </div>
+        </React.Fragment>
+    )
+}
 
 }
 

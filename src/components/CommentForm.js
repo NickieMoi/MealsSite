c609c4d53ed3e1b989163b4ccc6d12362a2bd809
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-28 15:44:25 +0300

    adds the comment form file component

---
+++ src/components/CommentForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CommentForm extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div>Comment form</div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default CommentForm

3c847db7f66208e1fb2efbe2fb8d0ee908006f4b
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-29 19:29:15 +0300

    adds the pics display component

---
+++ src/components/SingleItem.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+export default function MealCard({mealName, mealThumbnail, mealId,onDetailsClick,meal}){
+    function handleOnClick(){
+        onDetailsClick(meal)
+    }
+    return (
+        <div className="card" style={{width: 18 + 'rem'}}>
+            <img src={mealThumbnail} className="card-img-top" alt={mealName} style={{height:"200px"}}/>
+            <div className="card-body">
+                <h5 className="card-title">{mealName}</h5>
+                <Link to={"/meal/"+mealId}onClick={handleOnClick} className="btn btn-primary">VIEW DETAILS</Link>
+            </div>
+        </div>
+    )
+}
